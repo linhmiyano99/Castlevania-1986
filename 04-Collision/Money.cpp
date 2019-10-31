aa0bb58f94afd935d9eed371bca9489f3224c064
linhmiyano99 <32361868+linhmiyano99@users.noreply.github.com>
2019-10-31 16:34:01 +0700

    create class money

---
+++ 04-Collision/Money.cpp
@@ -0,0 +1,10 @@
+#include "Monney.h"
+
+void CMonney::GetBoundingBox(float& left, float& top, float& right, float& bottom)
+{
+
+}
+void CMonney::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
+{
+
+}
