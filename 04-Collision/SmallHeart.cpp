1f6ceeda6876eb25b7542986ef68c1435cebe914
linhmiyano99 <32361868+linhmiyano99@users.noreply.github.com>
2019-10-31 16:34:17 +0700

    create class small heart

---
+++ 04-Collision/SmallHeart.cpp
@@ -0,0 +1,10 @@
+#include "SmallHeart.h"
+
+void CSmallHeart::GetBoundingBox(float& left, float& top, float& right, float& bottom)
+{
+
+}
+void CSmallHeart::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
+{
+
+}
---
+++ 04-Collision/SmallHeart.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "Item.h"
+
+class CSmallHeart :public CItem
+{
+public:
+	CSmallHeart():CItem()
+	{
+
+	}
+	void GetBoundingBox(float& left, float& top, float& right, float& bottom);
+	virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects = NULL);
+
+};
